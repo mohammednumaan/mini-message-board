b1ce189d7bd4ed6071d5a279d3aa8a6311c71818
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2024-02-17 16:06:38 +0530

    code: implemented a way for users to send message

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('form')
+})
+
+router.post('/new', (req, res, next) => {
+  messages.push({text : req.body.userMessage, user: req.body.userName, added: new Date()})
+  res.redirect('/')
+})
+
 module.exports = router;
